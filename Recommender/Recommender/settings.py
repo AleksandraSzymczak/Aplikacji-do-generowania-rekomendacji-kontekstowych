438104bbc2a8756d0284fe25b2d5fb39231c0c4c
aleks <aleksis550@wp.pl>
2023-12-03 14:52:35 +0100

    add requirements allow all hosts

--- Recommender/Recommender/settings.py
+++ Recommender/Recommender/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-on1hn1%sqna31$@a1+xce_=eky@+w75wpw7#nk*^vqrz9g86(7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
