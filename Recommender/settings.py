431a311fd2fa9c2c1430773ffac860207423f4dc
aleks <aleksis550@wp.pl>
2023-12-06 21:42:35 +0100

    Change middleware order

--- Recommender/settings.py
+++ Recommender/settings.py
@@ -52,16 +52,17 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'account.CustomUser'
 
 MIDDLEWARE = [
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
     'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
