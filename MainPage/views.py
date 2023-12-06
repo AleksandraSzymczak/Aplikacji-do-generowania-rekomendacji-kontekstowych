6fa6bf947937a280d40e59352fcf08bceddb02e0
aleks <aleksis550@wp.pl>
2023-12-06 21:25:45 +0100

    Change views to detect user

--- MainPage/views.py
+++ MainPage/views.py
@@ -10,15 +10,16 @@ import json
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class HomeView(View):
+class HomeView(View, LoginRequiredMixin):
     def get(self, request):
-        #current_user = request.user
-        #pliki = Files.objects.filter(user=current_user).order_by('-uploaded_at').values_list('file', flat=True)
-        #substring_to_remove = "user_files/"
-        #result_list = [full_path.replace(substring_to_remove, "", 1) for full_path in pliki] 
-        return render(request, 'MainPage/main.html')#, #{'pliki': result_list})
+        current_user = request.user
+        pliki = Files.objects.filter(user=current_user).order_by('-uploaded_at').values_list('file', flat=True)
+        substring_to_remove = "user_files/"
+        result_list = [full_path.replace(substring_to_remove, "", 1) for full_path in pliki] 
+        return render(request, 'MainPage/main.html', {'pliki': result_list})
 
 
 
