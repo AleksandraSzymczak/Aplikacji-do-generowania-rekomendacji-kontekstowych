de6c3a845c465e4fe6a523af5080e0bb3d96e5aa
aleks <aleksis550@wp.pl>
2023-12-07 20:43:48 +0100

    add CSRF

--- Recommender/settings.py
+++ Recommender/settings.py
@@ -148,18 +148,27 @@ SIMPLE_JWT = {
 
 
 WSGI_APPLICATION = "Recommender.wsgi.application"
-
+CSRF_TRUSTED_ORIGINS = ['https://contextrecommendationapp.azurewebsites.net']
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'recommendserver',
+        'USER': 'rekomendacja',
+        'PASSWORD': 'qwerty1234!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
